e5c7ab61d01ebc24c3473a6659cdbac82db3b66d
Rahul Joshi <48386687+17J@users.noreply.github.com>
2025-04-23 13:44:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,13 @@
+# Azure Region
 variable "location" {
-  description = "Azure region"
+  description = "Azure region where resources will be deployed"
+  type        = string
   default     = "East US"
+}
+
+# SSH Public Key
+variable "ssh_public_key" {
+  description = "SSH public key for the AKS node access"
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD..."  # Replace with your real SSH key
 }

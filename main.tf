49ded085ff0c4a993ad1adbe526b72192b874d2d
Rahul Joshi <48386687+17J@users.noreply.github.com>
2025-04-23 13:38:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "devops17-sg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "devops17-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "devops17-subnet"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "devops17-aks"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "devopsshack"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}

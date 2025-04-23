af8530fdc1e7ff3b8eabf916634e3bda6bd0c656
Rahul Joshi <48386687+17J@users.noreply.github.com>
2025-04-23 13:43:56 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,21 @@
+# Output the raw Kube config (for kubectl access)
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
   sensitive = true
 }
 
+# Output the cluster host URL
+output "host" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+}
+
+# Output the client certificate (not usually needed manually)
 output "client_certificate" {
   value     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
   sensitive = true
 }
 
-output "host" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
-}
-
+# Output the created resource group
 output "resource_group_name" {
   value = azurerm_resource_group.aks_rg.name
 }
